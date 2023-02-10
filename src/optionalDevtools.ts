bed1cf26172bdffd071b98b2c69575af9020be03
arvinxx <arvinx@foxmail.com>
2023-02-10 20:06:24 +0800

    :bulb: chore: add some jsdoc comment for optionalDevtools

--- src/optionalDevtools.ts
+++ src/optionalDevtools.ts
@@ -2,9 +2,9 @@ import { devtools } from 'zustand/middleware';
 
 /**
  *
+ * Make devtools optional
  * 将是否开启 devtools 变成可选方案
- * 翻译：
- * Refs: https://github.com/pmndrs/zustand/discussions/1266
+ * @see https://github.com/pmndrs/zustand/discussions/1266
  */
 export const optionalDevtools = (showDevTools: boolean) =>
   (showDevTools ? devtools : (f) => f) as typeof devtools;
