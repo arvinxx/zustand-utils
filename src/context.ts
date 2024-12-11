b19ab77421418dc7fe356e3cef58b69c13f86bca
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2024-12-11 12:38:10 +0800

    feat: plan 2

--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import { createElement, createContext as reactCreateContext, useContext, useRef } from 'react';
 import type { StoreApi } from 'zustand';
+import { useStoreWithEqualityFn } from 'zustand/traditional';
 
 export type UseContextStore<S extends StoreApi<unknown>> = {
   (): ExtractState<S>;
@@ -40,9 +41,17 @@ export const createContext = <S extends StoreApi<unknown>>() => {
     selector?: (state: ExtractState<S>) => StateSlice,
     equalityFn?: (a: StateSlice, b: StateSlice) => boolean,
   ) => {
-    // todo: TS desfinition
-    // https://github.com/pmndrs/zustand/blob/v4.0.0/src/react.ts#L68-L71
-    return (useStoreApi() as unknown as any)(selector, equalityFn);
+    const store = useStoreApi();
+
+    // \\\\\\\\\\ v5 \\\\\\\\\\
+    if (typeof equalityFn === 'function') {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      return useStoreWithEqualityFn(store, selector as any, equalityFn);
+    }
+
+    // \\\\\\\\\\ v4 \\\\\\\\\\
+    // https://github.com/pmndrs/zustand/blob/v4.4.0/src/react.ts#L103-L106
+    return (store as unknown as any)(selector, equalityFn);
   };
 
   return {
